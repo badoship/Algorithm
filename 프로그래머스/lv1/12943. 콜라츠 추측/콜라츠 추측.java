1a4c3521f2d24bc4001eb40bd243529891d44040
badoship <46552290+badoship@users.noreply.github.com>
2023-01-02 18:57:04 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -2,12 +2,8 @@ class Solution {
     public int solution(long num) {
         int answer = 0;
         while(num > 1 && answer < 500){
+            num = (num % 2 == 0 ) ? num / 2 : num * 3 + 1;
             answer++;
-            if ( num % 2 == 0 ){
-                num /= 2;
-                continue;
-            }
-            num = num * 3 + 1;
         }
         return answer == 500 ? -1 : answer;
     }
