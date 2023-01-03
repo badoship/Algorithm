2442c3581fa96b39e963b3b237782d1561b57415
badoship <46552290+badoship@users.noreply.github.com>
2023-01-03 18:14:58 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.11 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -2,14 +2,7 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] arr) {
         if ( arr.length == 1 ) return new int[]{-1};
-        int[] answer = new int[arr.length-1];
-        int idx = 0;
         int minVal = Arrays.stream(arr).min().getAsInt();
-        for(int i : arr){
-            if(minVal != i){
-                answer[idx++] = i;
-            }
-        }
-        return answer;
+        return Arrays.stream(arr).filter(val->val != minVal).toArray();
     }
 }
