9665d009fed1c39560108b3c5fe8ff154e557a12
badoship <46552290+badoship@users.noreply.github.com>
2023-01-03 17:43:19 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 10.43 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/README.md
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 서울에서 김서방 찾기 - 12919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919?language=java) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 9.34 ms
+메모리: 82.9 MB, 시간: 10.43 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,6 +1,11 @@
 import java.util.Arrays;
 class Solution {
     public String solution(String[] seoul) {
-        return "김서방은 " + Arrays.asList(seoul).indexOf("Kim") +"에 있다";
+        int idx = 0;
+        for(String s : seoul){
+            if(s.equals("Kim")) return "김서방은 " + idx + "에 있다";
+            idx++;
+        }
+        return "";
     }
 }
