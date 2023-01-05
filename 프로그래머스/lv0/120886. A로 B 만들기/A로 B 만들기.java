4851247300257c19caf5c7f04ffa108fbfeb303f
badoship <46552290+badoship@users.noreply.github.com>
2023-01-05 15:38:46 +0900

    [level 0] Title: A로 B 만들기, Time: 0.31 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Comparator;
+class Solution {
+    public int solution(String before, String after) {
+        String[] beforeArr = before.split("");
+        String[] afterArr = after.split("");
+        Arrays.sort(beforeArr);
+        Arrays.sort(afterArr);
+        for(int i=0; i<beforeArr.length; i++){
+            if (!beforeArr[i].equals(afterArr[i])){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
