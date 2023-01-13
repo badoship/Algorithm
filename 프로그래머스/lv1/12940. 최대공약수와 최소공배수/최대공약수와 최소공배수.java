0d4090612eaa94faec32898e10ed770299e67c9f
badoship <46552290+badoship@users.noreply.github.com>
2023-01-14 00:56:52 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,7 +1,13 @@
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
-        answer[1] = n * m;
+        int gcd = gcd(n,m);
+        answer[0] = gcd;
+        answer[1] = n * m / gcd; 
+        return answer;
+    }
+    
+    public int gcd(int n, int m){
         int temp;
         if( n < m ){
             temp = n;
@@ -13,8 +19,6 @@ class Solution {
             n = m;
             m = temp;
         }
-        answer[0] = n;
-        answer[1] /= n; 
-        return answer;
+        return n;
     }
 }
