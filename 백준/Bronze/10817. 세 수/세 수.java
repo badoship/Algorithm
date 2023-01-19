27b52435804dc0b39e1e1c5d0e615a3444e9ca7c
badoship <46552290+badoship@users.noreply.github.com>
2023-01-19 22:22:05 +0900

    [Bronze III] Title: 세 수, Time: 208 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -5,13 +5,11 @@ public class Main{
     
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int[] intArr = new int[]{a,b,c};
+        int[] intArr = new int[]{sc.nextInt(),sc.nextInt(),sc.nextInt()};
+        sc.close();
         Arrays.sort(intArr);
         System.out.println(intArr[1]);
         
-        sc.close();
+        
     }
 }
