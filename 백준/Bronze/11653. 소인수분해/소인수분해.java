f046fc1c6860ef2eb8eb82139ac07dfa90e9a044
badoship <46552290+badoship@users.noreply.github.com>
2023-01-19 22:37:00 +0900

    [Bronze I] Title: 소인수분해, Time: 208 ms, Memory: 17756 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -6,12 +6,16 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
-        for(int i=2; i <= n; i++){
+        for(int i=2; i <= Math.sqrt(n); i++){
             
             while ( n % i == 0 ){
                 System.out.println(i);
                 n /= i;
             }
+        }
+        
+        if ( n != 1 ){
+            System.out.println(n);
         }
     }
 }
