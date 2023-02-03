83f9df80226b873eb935a1689891ac97704265d1
badoship <46552290+badoship@users.noreply.github.com>
2023-02-03 22:42:47 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -6,20 +6,19 @@ board = [list(input()) for _ in range(n)]
 
 
 def calDiffPattern(i):
-    cnt = 0
+    cnt1 = cnt2 = 0
     for y in range(0, 8):
         for x in range(0, 8):
             if pattern1[(i + y) % 2][x] != board[row + y][col + x]:
-                cnt += 1
+                cnt1 += 1
+            else :
+                cnt2 += 1
 
-                if cnt > minVal:
-                    return cnt
-    return cnt
+    return min(cnt1,cnt2)
 
 
 for row in range(0, n - 7):
     for col in range(0, m - 7):
-        for i in range(2):
-            minVal = min(minVal, calDiffPattern(i))
+        minVal = min(minVal, calDiffPattern(0))
 
 print(minVal)
