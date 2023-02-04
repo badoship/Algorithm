776624b6d8d5402e8c1e3333e0b13a2186fa32a5
badoship <46552290+badoship@users.noreply.github.com>
2023-02-04 21:30:49 +0900

    [Silver V] Title: 나이순 정렬, Time: 328 ms, Memory: 58564 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -4,8 +4,7 @@ case = int(input())
 ageNames = list()
 for i in range(case):
     ageNames.append(list(map(str,sys.stdin.readline().rstrip().split())))
-    ageNames[-1].append(i)
 
-ageNames.sort(key=lambda x:(int(x[0]),x[2]))
-for age , name , _ in ageNames :
+ageNames.sort(key=lambda x:int(x[0]))
+for age , name in ageNames :
     print(age,name)
