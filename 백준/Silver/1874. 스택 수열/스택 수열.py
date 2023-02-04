cb8f248cb5afcb56a9402ce584bd940f5ad6e305
badoship <46552290+badoship@users.noreply.github.com>
2023-02-04 22:17:12 +0900

    [Silver II] Title: 스택 수열, Time: 204 ms, Memory: 32840 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -2,30 +2,22 @@ import sys
 
 case = int(input())
 stack = []
-for _ in range(case):
-    stack.append(int(sys.stdin.readline().rstrip()))
-
-arr = []
 start = 1
 answer = []
-for s in stack :
+for _ in range(case):
+    num = int(sys.stdin.readline().rstrip())
 
-    while True :
-        if not arr :
-            arr.append(start)
-            start += 1
-            answer.append("+")
-        elif arr[-1] == s :
-            arr.pop()
-            answer.append("-")
-            break
-        elif arr[-1] < s :
-            arr.append(start)
-            answer.append("+")
-            start += 1
-        elif arr[-1] > s :
-            print("NO")
-            exit()
+    while not stack or stack[-1] < num :
+        answer.append("+")
+        stack.append(start)
+        start += 1
+
+    if stack[-1] == num :
+        answer.append("-")
+        stack.pop()
+    else :
+        print("NO")
+        exit()
 
 for ans in answer :
     print(ans)
