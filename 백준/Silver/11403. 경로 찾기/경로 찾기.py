44847a50dba28f674ac0919c9db1a459c9682ed3
badoship <46552290+badoship@users.noreply.github.com>
2023-02-07 19:13:06 +0900

    [Silver I] Title: 경로 찾기, Time: 136 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,13 +1,22 @@
 import sys
+from collections import deque
 
 n = int(input())
 graph = [list(map(int,sys.stdin.readline().rstrip().split())) for _ in range(n)]
 
-for middle in range(n):
-    for start in range(n):
-        for end in range(n):
-            if graph[start][middle] and graph[middle][end] :
-                graph[start][end] = 1
+for i in range(n) :
+    queue = deque()
+    queue.append(i)
+    visited = [0] * n
 
-for g in graph :
-    print(*g)
+    while queue :
+        q = queue.popleft()
+
+        for j in range(n):
+            if visited[j] == 0 and graph[q][j] == 1:
+                queue.append(j)
+                graph[i][j] = 1
+                visited[j] = 1
+
+for grp in graph :
+    print(*grp)
