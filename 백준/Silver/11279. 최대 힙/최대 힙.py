2f00d944808537756fc60c1567dc74e2f5ff435d
badoship <46552290+badoship@users.noreply.github.com>
2023-02-20 16:33:29 +0900

    [Silver II] Title: 최대 힙, Time: 164 ms, Memory: 46396 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,14 @@
+import heapq as hq
+import sys
+
+n = int(input())
+heap = []
+hq.heapify(heap)
+
+for _ in range(n):
+    k = int(sys.stdin.readline().rstrip())
+    if k == 0 :
+        print(hq.heappop(heap)[1] if heap else 0)
+        continue
+
+    hq.heappush(heap,(-k,k))
